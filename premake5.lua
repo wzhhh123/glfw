07cb16ed70fbce281192b219d120c0f46ac1ac83
unknown <wengzihao@game.ntes>
2020-10-15 17:36:42 +0800

    修改premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "on"
+	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
